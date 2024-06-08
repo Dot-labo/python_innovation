54144c7f4c5f658323a1dd1354fa80dc09acb6dc
kato-mahiro <kato.mahiro.3@gmail.com>
2024-06-08 18:44:14 +0900

    remove lesson04

--- 04_cmder_task/mission_ミッション/2_魔法使いの図書館/spell.py
+++
@@ -1,9 +0,0 @@
-# このプログラムは魔法の呪文を計算します。
-
-# TODO: 呪文の強さをランダムにする
-# magic_strength = random.randint(1, 10)
-magic_strength = 5
-
-print(f'魔法の強さは {magic_strength} です！')
-
-# 表示する魔法の強さは固定ですが、TODOにあるように将来的にランダムにする予定です。
