f3bb96971f907b43a432f3ceccd9745fd4b034af
kato-mahiro <kato.mahiro.3@gmail.com>
2024-06-16 13:17:03 +0900

    petのコード(ヒント)を追加

---
+++ python-basic/class_example_pet.py
@@ -0,0 +1,17 @@
+"""
+やってみよう(復習)のヒントです。
+"""
+
+#新しく Pet という名前のクラスを作りましょう。
+#クラスには、animal_typeというメンバ変数を持たせましょう。
+class Pet:
+    def __init__(self, ???):
+        self.???
+
+#Petクラスのインスタンスp1とp2を作りましょう。
+#p1のanimal_typeは 'イヌ' で、p2のanimal_typeは 'サル' です。
+p1 = 
+p2 =
+
+print("p1は", p1.animal_type, "です") #p1は イヌ です
+print("p2は", p2.animal_type, "です") #p2は サル です
